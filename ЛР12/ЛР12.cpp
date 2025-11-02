80ccdcd673773b5fde32e8d127d1db5a68e5f8a2
Victoria-Mykhailneko <vickysya2018@gmail.com>
2025-11-02 15:19:03 +0200

    Add project files.

---
+++ ЛР12/ЛР12.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cmath>  // для sqrt() і atan()
+
+using namespace std;
+
+// Функція для обчислення трьох результатів
+void calculate(double p, double t, double& k, double& x, double& y) {
+    k = sqrt(p + t * t);
+    x = p + k;
+    y = atan(x * x);
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double p, t;
+    cout << "Введіть значення p: ";
+    cin >> p;
+    cout << "Введіть значення t: ";
+    cin >> t;
+
+    double k, x, y;
+    calculate(p, t, k, x, y);
+
+    cout << "\nРезультати обчислень:" << endl;
+    cout << "k = " << k << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
+}
